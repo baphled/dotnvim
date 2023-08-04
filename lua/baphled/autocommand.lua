e6a81cc1dc1a3d8b3f9582507894fb65be2e01e1
Yomi Colledge <baphled@boodah.net>
2023-08-04 20:41:23 +0100

    fix(autocommand): remove duplication
    We use an autocommand for locally adding word wrap and spelling to our
    commit messages. As we were moving things around we ended up having the
    autocommand defined twice.
    
    Here we make sure that we hony have one definition.
    
    Signed-off-by: Yomi Colledge <baphled@boodah.net>

--- lua/baphled/autocommand.lua
+++ lua/baphled/autocommand.lua
@@ -1,16 +1,5 @@
 vim.api.nvim_create_augroup("_editing", { clear = true })
 
--- Enable spell check and word wrap for certain file types
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  group = "_editing",
-  pattern = { "gitcommit", "markdown", "txt" },
-  desc = "Enable spell checking and text wrapping for certain filetypes",
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
-})
-
 -- Prevent IndentLine from hiding ``` in markdown files
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = "_editing",
