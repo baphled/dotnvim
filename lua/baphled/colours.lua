feb35d65eafe09c7079fab1896eac98b4c886da1
Yomi Colledge <baphled@boodah.net>
2024-04-27 23:26:29 +0100

    chore(lsp-zero): Update with Lazy in mind
    Now that we've introduced Lazy, we've not some _slight_ issues that need
    to be resolved. Firstly, we need to migrate from using `lsp-zero` for
    handling languages to `Mason`. Next we had some issues with `lua_ls`
    throwing some errors.
    
    Here, we make sure that our config is up to date.
    
    Signed-off-by: Yomi Colledge <baphled@boodah.net>

--- lua/baphled/colours.lua
+++ lua/baphled/colours.lua
@@ -1,25 +1,3 @@
 -- LSP colours
 vim.cmd([[
-hi LspDiagnosticsDefaultError ctermfg=Red guifg=#F70067
-hi LspDiagnosticsDefaultWarning ctermfg=Yellow guifg=#FFEC63
-hi LspDiagnosticsDefaultInformation ctermfg=Blue guifg=#00B4F9
-hi LspDiagnosticsDefaultHint ctermfg=Cyan guifg=#00f1f5
-hi LspDiagnosticsUnderlineError ctermfg=Red guifg=#F70067 gui=underline
-hi LspDiagnosticsUnderlineWarning ctermfg=Yellow guifg=#FFEC63 gui=underline
-hi LspDiagnosticsUnderlineInformation ctermfg=Blue guifg=#00B4F9 gui=underline
-hi LspDiagnosticsUnderlineHint ctermfg=Cyan guifg=#00f1f5 gui=underline
-hi LspDiagnosticsFloatingError ctermfg=Red guifg=#F70067
-hi LspDiagnosticsFloatingWarning ctermfg=Yellow guifg=#FFEC63
-hi LspDiagnosticsFloatingInformation ctermfg=Blue guifg=#00B4F9
-hi LspDiagnosticsFloatingHint ctermfg=Cyan guifg=#00f1f5
-hi LspDiagnosticsSignError ctermfg=Red guifg=#F70067
-hi LspDiagnosticsSignWarning ctermfg=Yellow guifg=#FFEC63
-hi LspDiagnosticsSignInformation ctermfg=Blue guifg=#00B4F9
-hi LspDiagnosticsSignHint ctermfg=Cyan guifg=#00f1f5
-
-hi LspReferenceText ctermfg=Grey guifg=#8B8B8B
-hi LspReferenceRead ctermfg=Grey guifg=#8B8B8B
-hi LspReferenceWrite ctermfg=Grey guifg=#8B8B8B
-
-hi NotifyBackground ctermbg=White guibg=#FFFFFF
 ]])
--- lua/baphled/config/lsp-zero.lua
+++ lua/baphled/config/lsp-zero.lua
@@ -1,30 +1,213 @@
 local lsp_zero = require('lsp-zero')
-local lspkind = require('lspkind')
+local util = require("lspconfig.util")
 
-require("luasnip.loaders.from_vscode").lazy_load()
+require("mason").setup({})
 
-require("mason").setup {
+local capabilities = vim.tbl_deep_extend("force",
+  vim.lsp.protocol.make_client_capabilities(),
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+require('mason-lspconfig').setup({
   ensure_installed = {
-    'rust_analyzer',
-    'cssls',
-    'clangd',
     'arduino_language_server',
-    'solargraph',
-    'lua_ls',
-    'ruby_ls',
-    'tsserver',
+    'clangd',
+    'cssls',
     'eslint',
-    'vuels',
+    'lua_ls',
+    'ruby_lsp',
+    'rust_analyzer',
+    'solargraph',
+    'tsserver',
     'volar',
+    'vuels',
   },
-  handlers = { lsp_zero.default_setup }
-}
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+
+    --- Ruby
+
+    solargraph = function()
+      require('lspconfig').solargraph.setup({
+        on_attach = lsp_zero.on_attach,
+        capabilities = capabilities,
+        settings = {
+          solargraph = {
+            ignore = {
+              "node_modules/**",
+              "vendor/**",
+              "tmp/**",
+              ".bundle/**",
+            },
+            definitions = true,
+            diagnostics = true,
+            completion = true,
+            formatting = true,
+            rename = true,
+            useBundler = true,
+            autoformat = false,
+            autocomplete = true,
+          }
+        }
+      })
+    end,
+
+    ruby_lsp = function()
+      require('lspconfig').ruby_lsp.setup({
+        on_attach = lsp_zero.on_attach,
+        capabilities = capabilities,
+        commands = {
+          FormatRuby = {
+            function()
+              vim.lsp.buf.format({
+                name = "ruby_lsp",
+                async = true,
+              })
+            end,
+            description = "Format using ruby-lsp",
+          },
+        },
+      })
+    end,
+
+    --- Vue
+    vuels = function()
+      require('lspconfig').vuels.setup({
+        on_attach = lsp_zero.on_attach,
+        capabilities = capabilities,
+        filetypes = { "vue", "javascript" },
+        init_options = {
+          config = {
+            vetur = {
+              completion = {
+                autoImport = true,
+                tagCasing = "kebab",
+              },
+              useWorkspaceDependencies = true,
+              validation = {
+                template = true,
+                style = true,
+                script = true,
+              },
+              format = {
+                defaultFormatter = {
+                  js = "prettier",
+                  ts = "prettier",
+                },
+              },
+              script = {
+                indent = {
+                  base = 0,
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
+
+    --- TypeScript
+    tsserver = function()
+      require('lspconfig').tsserver.setup({
+        on_attach = lsp_zero.on_attach,
+        capabilities = capabilities,
+        filetypes = { "typescript", "typescriptreact" },
+        init_options = {
+          preferences = {
+            importModuleSpecifierPreference = "relative",
+          },
+          format = {
+            enable = true,
+            options = {
+              tabSize = 2,
+              insertSpaces = true,
+              singleQuote = true,
+              trailingComma = "all",
+            },
+          },
+        },
+      })
+    end,
+
+    --- JavaScript
+    eslint = function()
+      require('lspconfig').eslint.setup({
+        on_attach = function(_, bufnr)
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            command = "EslintFixAll",
+          })
+        end,
+        capabilities = capabilities,
+        settings = {
+          codeAction = {
+            disableRuleComment = {
+              enable = true,
+              location = "separateLine"
+            },
+            showDocumentation = {
+              enable = true
+            }
+          },
+          codeActionOnSave = {
+            enable = false,
+            mode = "all"
+          },
+          experimental = {
+            useFlatConfig = false
+          },
+          format = true,
+          nodePath = "",
+          onIgnoredFiles = "off",
+          problems = {
+            shortenToSingleLine = false
+          },
+          quiet = false,
+          rulesCustomizations = {},
+          run = "onType",
+          useESLintClass = false,
+          validate = "on",
+          workingDirectory = {
+            mode = "location"
+          }
+        }
+      })
+    end,
+
+    clangd = function()
+      require('lspconfig').clangd.setup({
+        on_attach = lsp_zero.on_attach,
+        capabilities = capabilities,
+        cmd = { "clangd", "--background-index" },
+        filetypes = { "c", "cpp", "objc", "objcpp" },
+        root_dir = util.root_pattern("compile_commands.json", ".git"),
+        settings = {
+          clangd = {
+            fileStatus = true,
+            semanticHighlighting = true,
+            fallbackFlags = {
+              "-std=c++17",
+              "-stdlib=libc++",
+              "-I/usr/include/c++/v1",
+              "-I/usr/local/include",
+              "-I/usr/include",
+            },
+          },
+        },
+      })
+    end,
+  }
+})
 
 lsp_zero.on_attach(function(_, _)
 end)
-
-local cmp = require('cmp')
-local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
 
 lsp_zero.set_sign_icons({
   error = " ",
@@ -33,253 +216,10 @@ lsp_zero.set_sign_icons({
   info = " ",
 })
 
-local lspconfig = require('lspconfig')
-
 require('lspconfig-bundler').setup()
 
 vim.diagnostic.config({
   virtual_text = true
-})
-
-local configs = require("lspconfig.configs")
-local util = require("lspconfig.util")
-
-local capabilities = vim.tbl_deep_extend("force",
-  vim.lsp.protocol.make_client_capabilities(),
-  require('cmp_nvim_lsp').default_capabilities()
-)
-
-lspconfig.cssls.setup({
-  on_attach = lsp_zero.on_attach,
-  capabilities = capabilities,
-})
-
-lspconfig.volar.setup({
-  on_attach = lsp_zero.on_attach,
-  capabilities = capabilities,
-  filetypes = { "vue" },
-})
-
-lspconfig.tsserver.setup({
-  on_attach = lsp_zero.on_attach,
-  capabilities = capabilities,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "javascript.jsx" },
-  typescript = {
-    codeActions = {
-      organizeImports = true,
-    },
-    codeLens = {
-      references = true,
-      rename = true,
-      organizeImports = true,
-    },
-    updateImportsOnFileMove = {
-      enabled = 'always',
-    },
-    inlayHints = {
-      includeInlayParameterNameHints = "all",
-      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-      includeInlayFunctionParameterTypeHints = true,
-      includeInlayVariableTypeHints = true,
-      includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-      includeInlayPropertyDeclarationTypeHints = true,
-      includeInlayFunctionLikeReturnTypeHints = true,
-      includeInlayEnumMemberValueHints = true,
-    },
-  },
-  javascript = {
-    codeActions = {
-      organizeImports = true,
-    },
-    codeLens = {
-      references = true,
-      rename = true,
-      organizeImports = true,
-    },
-    updateImportsOnFileMove = {
-      enabled = 'always',
-    },
-    inlayHints = {
-      includeInlayParameterNameHints = "all",
-      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-      includeInlayFunctionParameterTypeHints = true,
-      includeInlayVariableTypeHints = true,
-      includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-      includeInlayPropertyDeclarationTypeHints = true,
-      includeInlayFunctionLikeReturnTypeHints = true,
-      includeInlayEnumMemberValueHints = true,
-    },
-  },
-  root_dir = function() return vim.loop.cwd() end       -- run lsp for javascript in any directory
-})
-
-lspconfig.solargraph.setup({
-  on_attach = lsp_zero.on_attach,
-  capabilities = capabilities,
-  settings = {
-    solargraph = {
-      ignore = {
-        "node_modules/**",
-        "vendor/**",
-        "tmp/**",
-        ".bundle/**",
-      },
-      definitions = true,
-      diagnostics = true,
-      completion = true,
-      formatting = true,
-      rename = true,
-      useBundler = true,
-      autoformat = false,
-      autocomplete = true,
-    }
-  }
-})
-
-if not configs.ruby_lsp then
-  local enabled_features = {
-    "documentHighlights",
-    "documentSymbols",
-    "codeActions",
-  }
-
-  configs.ruby_ls = {
-    default_config = {
-      cmd = { "ruby-lsp" },
-      filetypes = { "ruby" },
-      root_dir = util.root_pattern("Gemfile", ".git"),
-      init_options = {
-        enabledFeatures = enabled_features,
-      },
-      settings = {},
-    },
-    commands = {
-      FormatRuby = {
-        function()
-          vim.lsp.buf.format({
-            name = "ruby_ls",
-            async = true,
-          })
-        end,
-        description = "Format using ruby-lsp",
-      },
-    },
-  }
-end
-
-lspconfig.ruby_ls.setup({
-  on_attach = lsp_zero.on_attach,
-  capabilities = capabilities,
-})
-
-lspconfig.vuels.setup({
-  on_attach = lsp_zero.on_attach,
-  capabilities = capabilities,
-  filetypes = { "vue", "javascript" },
-  init_options = {
-    config = {
-      css = {},
-      emmet = {},
-      html = {
-        suggest = {}
-      },
-      javascript = {
-        format = {
-        }
-      },
-      stylusSupremacy = {},
-      typescript = {
-        format = {}
-      },
-      vetur = {
-        completion = {
-          autoImport = true,
-          tagCasing = "kebab",
-          useScaffoldSnippets = true
-        },
-        format = {
-          enable = false,
-          defaultFormatter = {
-            js = "none",
-            ts = "none"
-          },
-          defaultFormatterOptions = {},
-          scriptInitialIndent = false,
-          styleInitialIndent = false
-        },
-        validation = {
-          script = true,
-          style = true,
-          template = true
-        }
-      }
-    }
-  },
-})
-
-lspconfig.eslint.setup({
-  on_attach = function(_, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      command = "EslintFixAll",
-    })
-  end,
-  capabilities = capabilities,
-  settings = {
-    codeAction = {
-      disableRuleComment = {
-        enable = true,
-        location = "separateLine"
-      },
-      showDocumentation = {
-        enable = true
-      }
-    },
-    codeActionOnSave = {
-      enable = false,
-      mode = "all"
-    },
-    experimental = {
-      useFlatConfig = false
-    },
-    format = true,
-    nodePath = "",
-    onIgnoredFiles = "off",
-    problems = {
-      shortenToSingleLine = false
-    },
-    quiet = false,
-    rulesCustomizations = {},
-    run = "onType",
-    useESLintClass = false,
-    validate = "on",
-    workingDirectory = {
-      mode = "location"
-    }
-  }
-})
-
-lspconfig.lua_ls.setup({
-  on_attach = lsp_zero.on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-    },
-  },
-  root_dir = function() return vim.loop.cwd() end       -- run lsp for javascript in any directory
-})
-
-lspconfig.clangd.setup({
-  capabilities = capabilities,
-  on_attach = lsp_zero.on_attach,
-  checkUpdate = true,
-  filetypes = { "c", "cpp", "cuda", "proto" },
 })
 
 lsp_zero.preset({
@@ -298,95 +238,7 @@ lsp_zero.preset({
   },
 })
 
-
-cmp.setup({
-  sources = {
-    { name = 'luasnip' },
-    { name = 'nvim_lsp' },
-    {
-      name = "buffer",
-      option = {
-        get_bufnrs = function()
-          return vim.api.nvim_list_bufs()
-        end,
-      },
-    },
-    { name = "path" },
-    { name = "emoji" },
-    { name = "neorg" },
-    { name = "orgmode" },
-  },
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- Disable <Tab> and <S-Tab>, as they conflict with GitHub Copilot
-    -- It is still possible to navigate with Arrow Up and Arrow Down
-    ['<Tab>'] = cmp.config.disable,
-    ['<S-Tab>'] = cmp.config.disable,
-    ['<Enter>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = function()
-      if cmp.visible() then
-        cmp.close()
-      else
-        cmp.complete()
-      end
-    end,
-    ['<Esc>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.close()
-      else
-        cmp.complete()
-      end
-      vim.cmd('stopinsert')
-    end),
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    ['<C-p>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item(cmp_select_opts)
-      else
-        cmp.complete()
-      end
-    end),
-    ['<C-n>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_next_item(cmp_select_opts)
-      else
-        cmp.complete()
-      end
-    end),
-  }),
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-    fields = { 'abbr', 'menu', 'kind' },
-    -- mix format with lspkind icons and cmp kind labels
-    format = lspkind.cmp_format({
-      with_text = true,       -- show text along icon
-      menu = ({
-        nvim_lsp = "[LSP]",
-        luasnip = "[Snip]",
-        vim_dadbod_completion = "[DB]",
-        buffer = "[buf]",
-        nvim_lua = "[API]",
-        path = "[path]",
-      }),
-      maxwidth = 50,               -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...',       -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function(_, vim_item)
-        return vim_item
-      end
-    })
-  },
-})
+require('baphled.config.cmp')
 
 lsp_zero.setup()
 
