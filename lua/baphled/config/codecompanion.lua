2c7bbbe59ebf1e3040c2ee9a8cfc5c811fd528a1
Yomi Colledge <baphled@boodah.net>
2024-12-12 01:51:55 +0000

    chore(codecompanion): Tweak our setup
    Signed-off-by: Yomi Colledge <baphled@boodah.net>

--- lua/baphled/config/codecompanion.lua
+++ lua/baphled/config/codecompanion.lua
@@ -5,41 +5,18 @@ require("codecompanion").setup({
 
     },
     inline = {
-      adapter = "ollama",
+      adapter = "copilot",
     },
   },
-  adapters = {
-    llama3 = function()
-      return require("codecompanion.adapters").extend("ollama", {
-        name = "llama3", -- Give this adapter a different name to differentiate it from the default ollama adapter
-        schema = {
-          model = {
-            default = "llama3:latest",
-          },
-          num_ctx = {
-            default = 16384,
-          },
-          num_predict = {
-            default = -1,
-          },
-        },
-      })
-    end,
-    codestral = function()
-      return require("codecompanion.adapters").extend("ollama", {
-        name = "codestral", -- Give this adapter a different name to differentiate it from the default ollama adapter
-        schema = {
-          model = {
-            default = "codestral:latest",
-          },
-          num_ctx = {
-            default = 16384,
-          },
-          num_predict = {
-            default = -1,
-          },
-        },
-      })
-    end,
+  slash_command = {
+    ["files"] = {
+      callback = "strategies.chat.slash_commands.file",
+      description = "Insert a file",
+      opts = {
+        contains_code = true,
+        max_lines = 1000,
+        provider = "telescope",     -- default|telescope|mini_pick|fzf_lua
+      },
+    }
   },
 })
