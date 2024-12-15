3f3d394700aa3af64dafc10fb9d8f8baf050f85c
Yomi Colledge <baphled@boodah.net>
2024-12-15 10:27:17 +0000

    chore(snacks): Add textobjects
    Signed-off-by: Yomi Colledge <baphled@boodah.net>

--- lua/baphled/config/snacks.lua
+++ lua/baphled/config/snacks.lua
@@ -49,6 +49,83 @@ require("snacks").setup({
       refresh = 50, -- refresh at most every 50ms
     }
   },
+  ---@class snacks.scope.Config
+  ---@field max_size? number
+  ---@field enabled? boolean
+  scope = {
+    -- absolute minimum size of the scope.
+    -- can be less if the scope is a top-level single line scope
+    min_size = 2,
+    -- try to expand the scope to this size
+    max_size = nil,
+    edge = true,    -- include the edge of the scope (typically the line above and below with smaller indent)
+    siblings = false, -- expand single line scopes with single line siblings
+    -- what buffers to attach to
+    filter = function(buf)
+      return vim.bo[buf].buftype == ""
+    end,
+    -- debounce scope detection in ms
+    debounce = 30,
+    treesitter = {
+      -- detect scope based on treesitter.
+      -- falls back to indent based detection if not available
+      enabled = true,
+      ---@type string[]|false
+      blocks = {
+        "function_declaration",
+        "function_definition",
+        "method_declaration",
+        "method_definition",
+        "class_declaration",
+        "class_definition",
+        "do_statement",
+        "while_statement",
+        "repeat_statement",
+        "if_statement",
+        "for_statement",
+      },
+      -- these treesitter fields will be considered as blocks
+      field_blocks = {
+        "local_declaration",
+      },
+    },
+    -- These keymaps will only be set if the `scope` plugin is enabled.
+    -- Alternatively, you can set them manually in your config,
+    -- using the `Snacks.scope.textobject` and `Snacks.scope.jump` functions.
+    keys = {
+      ---@type table<string, snacks.scope.TextObject|{desc?:string}>
+      textobject = {
+        ii = {
+          min_size = 2, -- minimum size of the scope
+          edge = false, -- inner scope
+          treesitter = { blocks = false },
+          desc = "inner scope",
+        },
+        ai = {
+          min_size = 2, -- minimum size of the scope
+          treesitter = { blocks = false },
+          desc = "full scope",
+        },
+      },
+      ---@type table<string, snacks.scope.Jump|{desc?:string}>
+      jump = {
+        ["[i"] = {
+          min_size = 1, -- allow single line scopes
+          bottom = false,
+          edge = true,
+          treesitter = { enabled = false },
+          desc = "jump to top edge of scope",
+        },
+        ["]i"] = {
+          min_size = 1, -- allow single line scopes
+          bottom = true,
+          edge = true,
+          treesitter = { enabled = false },
+          desc = "jump to bottom edge of scope",
+        },
+      },
+    },
+  },
   quickfile = { enabled = true },
   statuscolumn = { enabled = true },
   lazygit = { configure = true },
