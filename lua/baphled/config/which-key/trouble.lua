b48633d3b041f6c4b1828eb5d06f27ff3d025a29
Yomi Colledge <baphled@boodah.net>
2024-12-15 10:23:32 +0000

    chore(Trouble): Update key bindings
    Signed-off-by: Yomi Colledge <baphled@boodah.net>

--- lua/baphled/config/which-key/trouble.lua
+++ lua/baphled/config/which-key/trouble.lua
@@ -6,13 +6,33 @@ which_key.add({
     group = "Trouble",
   },
   {
-    "<leader>xd",
-    "<cmd>Trouble diagnostics toggle focus=false filter.buf=0<cr>",
-    desc = "Find Document Diagnostics",
-  },
-  {
     "<leader>xx",
     "<cmd>Trouble diagnostics toggle<cr>",
-    desc = "Toggle Trouble",
+    desc = "Diagnostics (Trouble)",
+  },
+  {
+    "<leader>xX",
+    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+    desc = "Buffer Diagnostics (Trouble)",
+  },
+  {
+    "<leader>cs",
+    "<cmd>Trouble symbols toggle focus=false<cr>",
+    desc = "Symbols (Trouble)",
+  },
+  {
+    "<leader>cl",
+    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    desc = "LSP Definitions / references / ... (Trouble)",
+  },
+  {
+    "<leader>xL",
+    "<cmd>Trouble loclist toggle<cr>",
+    desc = "Location List (Trouble)",
+  },
+  {
+    "<leader>xQ",
+    "<cmd>Trouble qflist toggle<cr>",
+    desc = "Quickfix List (Trouble)",
   },
 })
