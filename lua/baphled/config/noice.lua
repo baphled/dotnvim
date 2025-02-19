fc6b4433d971bd80497a5ecc1e1cb1fd6841b42b
Yomi Colledge <baphled@boodah.net>
2025-02-19 13:16:35 +0000

    chore(noice): Tweak settings
    Signed-off-by: Yomi Colledge <baphled@boodah.net>

--- lua/baphled/config/noice.lua
+++ lua/baphled/config/noice.lua
@@ -6,13 +6,49 @@ require("noice").setup({
       ["vim.lsp.util.stylize_markdown"] = true,
       ["cmp.entry.get_documentation"] = true,
     },
+    hover = {
+      enabled = true,
+      silent = true,
+    },
+    signature = {
+      enabled = true,
+      silent = true,
+      trigger = true,
+      luasnip = true,
+      throttle = 50,
+    },
+    documentation = {
+      view = "hover",
+      ---@type NoiceViewOptions
+      opts = {
+        lang = "markdown",
+        replace = true,
+        render = "plain",
+        format = { "{message}" },
+        win_options = { concealcursor = "n", conceallevel = 3 },
+      },
+    },
+  },
+  markdown = {
+    hover = {
+      ["|(%S-)|"] = vim.cmd.help,                       -- vim help links
+      ["%[.-%]%((%S-)%)"] = require("noice.util").open, -- markdown links
+    },
+    highlights = {
+      ["|%S-|"] = "@text.reference",
+      ["@%S+"] = "@parameter",
+      ["^%s*(Parameters:)"] = "@text.title",
+      ["^%s*(Return:)"] = "@text.title",
+      ["^%s*(See also:)"] = "@text.title",
+      ["{%S-}"] = "@parameter",
+    },
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = false, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
+    bottom_search = false,        -- use a classic bottom cmdline for search
+    command_palette = true,       -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = true, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = true, -- add a border to hover docs and signature help
+    inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = true,        -- add a border to hover docs and signature help
   },
 })
