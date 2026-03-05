5e63b1e754060c342d4c4aae5b1403d703bc9e30
Yomi Colledge <baphled@boodah.net>
2026-03-05 23:07:53 +0000

    fix(markdown): Defer quarto activation until buffer is loaded
    - Wrap quarto activation in vim.defer_fn to ensure buffer is fully loaded
    - Prevents errors when markdown filetype is set during buffer initialization
    
    This fixes timing issues where quarto tries to activate before the buffer
    is completely ready.

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,1 +1,7 @@
-require("quarto").activate()
+-- Defer quarto activation until buffer is fully loaded
+vim.defer_fn(function()
+  local buf = vim.api.nvim_get_current_buf()
+  if vim.api.nvim_buf_is_loaded(buf) then
+    require("quarto").activate()
+  end
+end, 0)
